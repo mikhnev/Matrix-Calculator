b9105d100580436d3e81348753aca5a0c1995751
mikhnev <venden2309@gmail.com>
2019-09-24 12:51:20 +0300

    add files

---
+++ SizeChooser.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+LARGE_FONT = ("Verdana", 12)
+class SizeChooser(tk.Frame):
+
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.rows = tk.Entry(self)
+        self.rows.grid(row=0, column=0)
+        label = tk.Label(self, text=" x ", font=LARGE_FONT)
+        label.grid(row=0, column=1)
+        self.columns = tk.Entry(self)
+        self.columns.grid(row=0, column=2)
---
+++ project.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from Operations import *
+
+class Calc(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+        for F in (StartPage, Mult):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        for F in (StartPage, Sum):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+app = Calc()
+app.mainloop()
