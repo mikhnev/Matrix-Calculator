67b8a1be895e702e25cb52d135acbe325588d4fc
Vlad Orlov <vorlov@molnet.ru>
2019-09-24 16:26:54 +0300

    flake

--- Simple_operations.py
+++ Simple_operations.py
@@ -1,23 +1,30 @@
 import math
 import numpy as nm
 
+
 def do_mult(x, y):
     return nm.dot(x, y)
+
 
 def do_sum(x, y):
     return x+y
 
+
 def do_subtract(x, y):
     return x - y
+
 
 def do_transpose(x):
     return nm.transpose(x)
 
+
 def do_det(x):
     return nm.linalg.det(x)
 
+
 def do_inv(x):
     return nm.linalg.inv(x)
+
 
 def do_pow(x, n):
     return nm.linalg.matrix_power(x, n)
