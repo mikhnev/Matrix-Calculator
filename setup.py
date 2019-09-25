ddbf42b50b6040dfb703e5e74729bf2cd607f306
mikhnev <venden2309@gmail.com>
2019-09-26 00:01:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,6 @@ setuptools.setup(
     locale_src='./mcalc/loc',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
