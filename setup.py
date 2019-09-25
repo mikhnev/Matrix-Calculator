e0ad02593ba950f0cb19eac3691852d982553e38
mikhnev <venden2309@gmail.com>
2019-09-25 17:54:48 +0300

    create .po file

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Matrix-Calculator",
+    version="0.0.1",
+    author="Denis Mikhnev and Vlad Orlov",
+    author_email="denis.mikhnev@gmail.com",
+    description="Python3 project for cmc msu course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mikhnev/Matrix-Calculator",
+    packages=setuptools.find_packages(),
+    setup_require=["mo_installer"],
+    locale_src='.',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
